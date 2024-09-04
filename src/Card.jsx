14bc1acf0bc4eaa530abbe63783c90d5c6664731
Silvio <nirvash533@gmail.com>
2024-09-04 16:33:36 -0500

    Added some styling to the paage and added a fe lines to improve responsiveness

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,16 +3,16 @@ import './Card.css';
 
 function Card(props) {
   return (
-    
+    <div className="card">
       
-        <div className="card">
+        <div className="card-content">
           <h2>{props.name}</h2>
           <img src={props.img}></img>
           <p>Phone: {props.phone}</p>
           <p>Email: {props.email}</p>
         </div>
       
-    
+        </div>
   );
 }
 
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,14 +1,25 @@
 import React from "react";
-import "./App.css"
+import "./App.css";
 
-function Header(){
-    return(
-        <>
-        <header>
-           <h1>This is my contact list</h1> </header>
-        </>
-    );
-};
-
+function Header() {
+  return (
+    <>
+      <header>
+        <h1>This is my contact list</h1>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </header>
+    </>
+  );
+}
 
 export default Header;
