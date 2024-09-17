f363a9997ae410dc0bf1b1b7988759cb0fae8cb8
Silvio <nirvash533@gmail.com>
2024-09-17 16:01:50 -0500

    on the next step we proceed to turn the propieties of certain component into components themselves so they can be reused on other parts of the website

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Card.css";
+import Avatar from "./Avatar";
 
 function Card(props) {
   return (
     <div className="card">
       <div className="card-content">
         <h2>{props.name}</h2>
+        
+        <Avatar img={props.img}/>
         <img src={props.img}></img>
         <p>Phone: {props.phone}</p>
         <p>Email: {props.email}</p>
